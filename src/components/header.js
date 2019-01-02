986a92317a90eaea392c3618c073175550925a7e
airhorse83 <airhorse83@yahoo.com>
2019-01-02 16:29:26 -0700

    added new mailto link

--- src/components/header.js
+++ src/components/header.js
@@ -7,6 +7,7 @@ export default class Header extends Component {
         <h1>Honor.Integrity.Excellence</h1>
         <a href="https://www.linkedin.com/in/j-tyler-horsley-2a88a1173/"><i class="fab fa-linkedin"></i></a>
         <a href="https://github.com/airhorse83"><i class="fab fa-github-square"></i></a>
+        <a href="mailto:airhorse83@yahoo.com"><i class="fas fa-envelope-square"></i></a>
       </div>
     );
   }
