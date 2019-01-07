a2c6b30a60aa77bba4ed1bde7543b870053754fc
airhorse83 <airhorse83@yahoo.com>
2019-01-07 15:47:15 -0700

    live push

--- src/components/mission.js
+++ src/components/mission.js
@@ -10,7 +10,9 @@ export default class Mission extends Component {
       <div className='mission-card'>
          
          
-          <Typing>My name is Tyler and I am a full stack web developer.  I love solving problems with code.  I enjoy daily challanges and want to be a part of an ever growing and exciting industry where anything is possible.  Lets work together on a project.  Click the mail button above to email me.  I am available for freelance work at the moment.</Typing>
+          <Typing>
+            <span>My name is Tyler and I am a full stack web developer.  I love solving problems with code.  I enjoy daily challanges and want to be a part of an ever growing and exciting industry where anything is possible.  Lets work together on a project.  Click the mail button above to email me.  I am available for freelance work at the moment.</span>
+          </Typing>
           
         
       </div>
